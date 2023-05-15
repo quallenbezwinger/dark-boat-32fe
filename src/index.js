7d18ee8f6aa2712e6021a775f3971919de791809
quallenbezwinger <kontakt@dirkkoehler.com>
2023-05-15 09:03:46 +0200

    fix cookie handling

--- src/index.js
+++ src/index.js
@@ -2,17 +2,22 @@ import { parse } from "cookie";
 export default {
   async fetch(request) {
 
-    const newLocationHost = "developers.cloudflare.com/workers/about/";
     const COOKIE_NAME = "cf-noredir";
     const cookie = parse(request.headers.get("Cookie") || "");
+    const newLocationHost = "developers.cloudflare.com/workers/about/";
+
+    let noRedirect = false
+    if (cookie[COOKIE_NAME] != null) {
+      if (cookie[COOKIE_NAME] == "true")  {
+        noRedirect = true;
+      }
+    }
 
     let reqUA = request.headers.get('user-agent')
     if (reqUA.match('curl')!=null) {
-      if (cookie[COOKIE_NAME] != null) {
-        if (cookie[COOKIE_NAME] != "true")  {
-          let newLocation = "https://"+newLocationHost
-          return Response.redirect(newLocation, 302)
-        }
+      if (noRedirect != true) {
+        let newLocation = "https://"+newLocationHost
+        return Response.redirect(newLocation, 302)
       }
     }
     return fetch(request);
